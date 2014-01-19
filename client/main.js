a0371468d2ea465025fb31cb986e65de766d939d
Theodor Vararu <theo@tvararu.ro>
2014-01-19 18:51:04 +0200

    Add bootstrap-3 and iron-router

--- client/main.js
+++ client/main.js
@@ -1,2 +1,20 @@
-Meteor.subscribe('posts');
-Meteor.subscribe('answers');
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.map(function() {
+  this.route('home', {
+    path: '/',
+    template: 'home',
+    
+    before: function() {
+      this.subscribe('posts');
+      this.subscribe('answers');
+    }
+  });
+  
+  this.route('sup', {
+    path: '/sup',
+    template: 'sup'
+  });
+});
